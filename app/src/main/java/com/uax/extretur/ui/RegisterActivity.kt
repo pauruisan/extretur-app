9b0a9aab943a975953244cb3bce217204599013e
Paula Ruiz <pauruisan@outlook.es>
2025-04-14 13:38:23 +0200

    Añado activity para registro y su correspondiente layout

---
+++ app/src/main/java/com/uax/extretur/ui/RegisterActivity.kt
@@ -0,0 +1,19 @@
+package com.uax.extretur.ui
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.uax.extretur.R
+import com.uax.extretur.databinding.ActivityRegisterBinding
+
+class RegisterActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRegisterBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
