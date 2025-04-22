06fd276077d1e5786425827a88cb3a5694cb5962
Paula Ruiz <pauruisan@outlook.es>
2025-04-22 13:46:00 +0200

    Splash Screen configurada

--- app/src/main/java/com/uax/extretur/ui/SplashActivity.kt
+++ app/src/main/java/com/uax/extretur/ui/SplashActivity.kt
@@ -3,13 +3,19 @@ package com.uax.extretur.ui
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
+import com.uax.extretur.R
 
-@SuppressLint("CustomSplashScreen")
-class SplashActivity: AppCompatActivity() {
+@Suppress("DEPRECATION")
+class SplashActivity : AppCompatActivity() {
+    @SuppressLint("ResourceType")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        startActivity(Intent(this, MainActivity::class.java))
-        finish()
+
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 1800)
     }
 }
