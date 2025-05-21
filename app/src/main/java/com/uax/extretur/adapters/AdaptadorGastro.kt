072c1d23bdfdeee398a3af360697c20cfe236de8
Paula Ruiz <pauruisan@outlook.es>
2025-05-21 09:26:04 +0200

    Preparación intents

--- app/src/main/java/com/uax/extretur/adapters/AdaptadorGastro.kt
+++ app/src/main/java/com/uax/extretur/adapters/AdaptadorGastro.kt
@@ -1,6 +1,8 @@
 package com.uax.extretur.adapters
 
 import android.content.Context
+import android.content.Intent
+import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,8 +10,10 @@ import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.uax.extretur.R
 import com.uax.extretur.model.Gastro
+import com.uax.extretur.ui.DetailMonumentsActivity
 
 class AdaptadorGastro(var listaGastro: ArrayList<Gastro>, private var contexto: Context) :
     RecyclerView.Adapter<AdaptadorGastro.MyHolder>() {
@@ -28,11 +32,18 @@ class AdaptadorGastro(var listaGastro: ArrayList<Gastro>, private var contexto: 
     }
 
     override fun onBindViewHolder(holder: MyHolder, position: Int) {
-        val actividad = listaGastro[position]
-        holder.imagen.setImageResource(actividad.imagen)
-        holder.boton
-        holder.titulo.text = actividad.nombre
-        holder.descripcion.text = actividad.descripcion
+        val plato = listaGastro[position]
+        Glide.with(contexto).load(plato.imagen).placeholder(R.drawable.chef_hat_24px).error(R.drawable.chef_hat_24px).into(holder.imagen)
+        holder.boton.setOnClickListener {
+            val intent = Intent(contexto, DetailMonumentsActivity::class.java)
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            val bundle = Bundle()
+            bundle.putSerializable("plato", plato)
+            intent.putExtra("datos", bundle)
+            contexto.startActivity(intent)
+        }
+        holder.titulo.text = plato.nombre
+        holder.descripcion.text = plato.descripcion
     }
 
 
