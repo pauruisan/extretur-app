e087d1cdbbacc589bdb92e73cb5fff68f277a74d
Paula Ruiz <pauruisan@outlook.es>
2025-05-23 11:05:00 +0200

    Funcionalidad a imagen del perfil

--- app/src/main/java/com/uax/extretur/ui/ProfileActivity.kt
+++ app/src/main/java/com/uax/extretur/ui/ProfileActivity.kt
@@ -1,6 +1,7 @@
 package com.uax.extretur.ui
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.util.Log.e
@@ -9,6 +10,7 @@ import android.view.View
 import android.view.View.OnClickListener
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 import com.google.android.material.navigation.NavigationBarView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
@@ -23,6 +25,15 @@ class ProfileActivity : AppCompatActivity(), OnClickListener {
         binding = ActivityProfileBinding.inflate(layoutInflater)
         setContentView(binding.root)
         auth = FirebaseAuth.getInstance()
+
+        val prefs = getSharedPreferences("perfil", MODE_PRIVATE)
+        val uriString = prefs.getString("imageUri", null)
+        if (uriString != null) {
+            val imageUri = Uri.parse(uriString)
+            Glide.with(this)
+                .load(imageUri)
+                .circleCrop()
+                .into(binding.imgProfile)}
         acciones()
         userData()
 
@@ -67,6 +78,7 @@ class ProfileActivity : AppCompatActivity(), OnClickListener {
 
     private fun acciones() {
         binding.btnLogout.setOnClickListener(this)
+        binding.imgProfile.setOnClickListener(this)
     }
 
     private fun userData (){
@@ -97,6 +109,22 @@ class ProfileActivity : AppCompatActivity(), OnClickListener {
         }
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 100 && resultCode == RESULT_OK) {
+            val imageUri = data?.data
+            if (imageUri !=null){
+                val prefs = getSharedPreferences("perfil", MODE_PRIVATE)
+                prefs.edit().putString("imageUri", imageUri.toString()).apply()
+                Glide.with(this)
+                    .load(imageUri)
+                    .circleCrop()
+                    .into(binding.imgProfile)
+            }
+
+        }
+    }
+
     override fun onClick(v: View?) {
         when(v?.id){
             binding.btnLogout.id -> {
@@ -106,6 +134,11 @@ class ProfileActivity : AppCompatActivity(), OnClickListener {
                 val intent = Intent(applicationContext, LogInActivity::class.java)
                 startActivity(intent)
                 finish()
+            }
+            binding.imgProfile.id -> {
+                val intent = Intent(Intent.ACTION_PICK)
+                intent.type = "image/*"
+                startActivityForResult(intent, 100)
             }
         }
     }
